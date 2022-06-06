15fa14da842f0036eecc8686dd6d0989c0867946
Denniz18 <denniz.dev18@gmail.com>
2022-06-06 17:54:27 +0300

    remove rootSaga

--- src/store/rootSaga.js
+++
@@ -1,7 +0,0 @@
-import { all } from 'redux-saga/effects';
-import { tasksSaga } from './tasks/saga';
-import { subTasksSaga } from './subTasks/saga';
-
-export default function* () {
-  yield all([tasksSaga(), subTasksSaga()]);
-}
